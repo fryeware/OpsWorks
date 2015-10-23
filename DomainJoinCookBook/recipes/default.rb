0699db728432af78eb66e09fe2d1e8b0a81021d8
fryeware <frye3@llnl.gov>
2015-10-23 16:03:38 -0700

    synced file change to master.

--- DomainJoinCookBook/recipes/default.rb
+++ DomainJoinCookBook/recipes/default.rb
@@ -3,3 +3,8 @@ remote_file 'C:/Windows/Temp/EC2DomainJoin.exe' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-bootstrap/EC2DomainJoin.exe'
   action :create
 end
+Chef::Log.info("******executing domainjoin ******")
+execute 'EC2DomainJoin' do
+  command 'c:/Windows/Temp/EC2DomainJoin.exe'
+end
+Chef::Log.info("******domain join complete ******")
