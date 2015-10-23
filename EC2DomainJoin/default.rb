dd9b04e56eeea8e4a553879144d234b9bdb1c495
fryeware <frye3@llnl.gov>
2015-10-23 15:58:33 -0700

    added execute section to default ruby file.

---
+++ EC2DomainJoin/default.rb
@@ -0,0 +1,9 @@
+Chef::Log.info("******downloading domainjoin executable.******")
+remote_file 'C:/Windows/Temp/EC2DomainJoin.exe' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-bootstrap/EC2DomainJoin.exe'
+  action :create
+end
+Chef::Log.info("******executing domainjoin ******")
+execute 'EC2DomainJoin' do
+  command 'c:/Windows/Temp/EC2DomainJoin.exe'
+end
