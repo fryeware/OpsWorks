34ebcef3df54f128dbeb224e812a8d8582930235
fryeware <frye3@llnl.gov>
2015-10-24 10:55:12 -0700

    reference FQDN on remove.

--- Tools/RemoveSessionHost/Program.cs
+++ Tools/RemoveSessionHost/Program.cs
@@ -11,13 +11,13 @@ namespace RemoveSessionHost
     {
         static void Main(string[] args)
         {
-            string script = "Remove-RDSessionHost -SessionHost " + Environment.GetEnvironmentVariable("COMPUTERNAME") + " -ConnectionBroker safeweb-rdscb3.safeweb.aws.net -Force";
+            string script = "Remove-RDSessionHost -SessionHost " + Environment.GetEnvironmentVariable("COMPUTERNAME") + ".safeweb.aws.net -ConnectionBroker safeweb-rdscb3.safeweb.aws.net -Force";
             PowerShell ps = PowerShell.Create();
             ps.AddScript(script);
             ps.Invoke();
             ps.Dispose();
 
-            script = "Remove-RDServer -Server " + Environment.GetEnvironmentVariable("COMPUTERNAME") + " -Role RDS-RD-Server -ConnectionBroker safeweb-rdscb3.safeweb.aws.net -Force";
+            script = "Remove-RDServer -Server " + Environment.GetEnvironmentVariable("COMPUTERNAME") + ".safeweb.aws.net -Role RDS-RD-Server -ConnectionBroker safeweb-rdscb3.safeweb.aws.net -Force";
             ps = PowerShell.Create();
             ps.AddScript(script);
             ps.Invoke();
