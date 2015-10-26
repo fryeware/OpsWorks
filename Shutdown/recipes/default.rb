5ff31bab1bc230fb0bcc2c36797a3f232283449e
fryeware <frye3@llnl.gov>
2015-10-26 16:02:50 -0700

    consolidated web service

--- Shutdown/recipes/default.rb
+++ Shutdown/recipes/default.rb
@@ -3,6 +3,10 @@ remote_file 'C:/Windows/Temp/RemoveSessionHost.exe' do
   source 'http://172.31.31.252/apps/RemoveSessionHost.exe'
   action :create
 end
+remote_file 'C:/Windows/Temp/RemoveSessionHost.exe.config' do
+  source 'http://172.31.31.252/apps/RemoveSessionHost.exe.config'
+  action :create
+end
 Chef::Log.info("******executing safeweb shutdown code ******")
 execute 'shutdown' do
   command 'C:/Windows/Temp/RemoveSessionHost.exe'
--- Tools/RemoveSessionHost/Program.cs
+++ Tools/RemoveSessionHost/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Management.Automation;
+using System.Net;
 
 namespace RemoveSessionHost
 {
@@ -11,17 +12,26 @@ namespace RemoveSessionHost
     {
         static void Main(string[] args)
         {
-            string script = "Remove-RDSessionHost -SessionHost " + Environment.GetEnvironmentVariable("COMPUTERNAME") + ".safeweb.aws.net -ConnectionBroker safeweb-rdscb3.safeweb.aws.net -Force";
-            PowerShell ps = PowerShell.Create();
-            ps.AddScript(script);
-            ps.Invoke();
-            ps.Dispose();
+            //string script = "Remove-RDSessionHost -SessionHost " + Environment.GetEnvironmentVariable("COMPUTERNAME") + ".safeweb.aws.net -ConnectionBroker safeweb-rdscb3.safeweb.aws.net -Force";
+            //PowerShell ps = PowerShell.Create();
+            //ps.AddScript(script);
+            //ps.Invoke();
+            //ps.Dispose();
 
-            script = "Remove-RDServer -Server " + Environment.GetEnvironmentVariable("COMPUTERNAME") + ".safeweb.aws.net -Role RDS-RD-Server -ConnectionBroker safeweb-rdscb3.safeweb.aws.net -Force";
-            ps = PowerShell.Create();
-            ps.AddScript(script);
-            ps.Invoke();
-            ps.Dispose();
+            //script = "Remove-RDServer -Server " + Environment.GetEnvironmentVariable("COMPUTERNAME") + ".safeweb.aws.net -Role RDS-RD-Server -ConnectionBroker safeweb-rdscb3.safeweb.aws.net -Force";
+            //ps = PowerShell.Create();
+            //ps.AddScript(script);
+            //ps.Invoke();
+            //ps.Dispose();
+
+            //WebRequest request = WebRequest.Create("http://safeweb-wf1:8080/RemoveSafewebServer/" + Environment.GetEnvironmentVariable("COMPUTERNAME").ToLower() + ".safeweb.aws.net");
+            //request.Credentials = CredentialCache.DefaultCredentials;
+            //WebResponse response = request.GetResponse();
+
+            RDSHelper.RDSProvisioningClient rdsHelper = new RDSHelper.RDSProvisioningClient();
+            rdsHelper.RemoveMeLS(Environment.GetEnvironmentVariable("COMPUTERNAME") + ".safeweb.aws.net");
+
+
         }
     }
 }
