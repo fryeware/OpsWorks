2cd7dd0aca76ce36371ede88ea10970709f234c7
landmissle <clendenin1@llnl.gov>
2015-10-28 16:26:09 -0700

    WLS edits

---
+++ WLS/metadata.rb
@@ -0,0 +1,2 @@
+name "wls"
+version "0.1.0"
---
+++ WLS/recipes/default.rb
@@ -0,0 +1,20 @@
+Chef::Log.info("******downloading WLS ******")
+remote_file 'C:/Windows/Temp/WLS.exe' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/WLS/WLS.EXE'
+  action :create
+end
+Chef::Log.info("******executing WLS installer ******")
+execute 'WLS' do
+  command 'C:/Windows/Temp/WLS.exe'
+end
+Chef::Log.info("******WLS install complete ******")
+Chef::Log.info("******downloading WLS Configue ******")
+remote_file 'C:/Windows/Temp/WLS-SafewebConfig.reg' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/WLS/WLS-SafewebConfig.reg'
+  action :create
+end
+Chef::Log.info("******Configure WLS ******")
+execute 'WLSconfig' do
+  command 'C:/Windows/system32/reg.exe  /import C:/Windows/Temp/WLS-SafewebConfig.reg' 
+end
+Chef::Log.info("******WLS Configure Complete ******")
