6d7dd8159e53c65152b53bc031006222fd1d5dfc
landmissle <clendenin1@llnl.gov>
2015-10-27 17:47:24 -0700

    Test action

--- Firefox/recipes/default.rb
+++ Firefox/recipes/default.rb
@@ -1,4 +1,4 @@
-Chef::Log.info("******downloading firefox installer.******")
+Chef::Log.info("******downloading firefox installer.***4***")
 remote_file 'C:/Windows/Temp/FirefoxSetup.exe' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/LLNL_FF_38_3_0_ESR_wCCK.EXE'
   action :create
