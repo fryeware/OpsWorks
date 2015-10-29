e930fcd1f9929d96bf49bedc814a92d9c8727eae
landmissle <clendenin1@llnl.gov>
2015-10-29 14:40:56 -0700

    Added .exe to Quicktime file name download

--- Quicktime/recipes/default.rb
+++ Quicktime/recipes/default.rb
@@ -1,5 +1,5 @@
 Chef::Log.info("******downloading Quicktime player ******")
-remote_file 'C:/Windows/Temp/Quicktime' do
+remote_file 'C:/Windows/Temp/Quicktime.exe' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Quicktime.exe'
   action :create
 end
