4f1c6287ce3f1e48c1ceb92232280d6258d9e668
landmissle <clendenin1@llnl.gov>
2015-10-29 14:09:11 -0700

    Quicktime recipe created

---
+++ Quicktime/recipes/default.rb
@@ -0,0 +1,10 @@
+Chef::Log.info("******downloading Quicktime player ******")
+remote_file 'C:/Windows/Temp/Acrobat' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Quicktime.exe'
+  action :create
+end
+Chef::Log.info("******executing Quicktime installer ******")
+execute 'Quicktime' do
+  command 'C:/Windows/Temp/Quicktime.exe'
+end
+Chef::Log.info("****** Quicktime install complete ******")
