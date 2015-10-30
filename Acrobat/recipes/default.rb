96e7fb83e64e0283ab443cc9f5393c1d5d29e593
landmissle <clendenin1@llnl.gov>
2015-10-30 13:11:09 -0700

    Capitilization changes to acrobat.exe

--- Acrobat/recipes/default.rb
+++ Acrobat/recipes/default.rb
@@ -1,10 +1,10 @@
 Chef::Log.info("******downloading Acrobat Reader******")
 remote_file 'C:/Windows/Temp/Acrobat.exe' do
-  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Acrobat.exe'
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/acrobat.exe'
   action :create
 end
 Chef::Log.info("******executing Acrobat installer ******")
-execute 'Acrobat' do
-  command 'C:/Windows/Temp/Acrobat.exe'
+execute 'acrobat' do
+  command 'C:/Windows/Temp/acrobat.exe'
 end
 Chef::Log.info("******Acrobat install complete ******")
