b2804fa2a2d076c3b7a5d6040c53c38e07d66758
landmissle <clendenin1@llnl.gov>
2015-10-30 09:13:24 -0700

    Removed capitlization from shockwave path in S3 path

--- Shockwave/recipes/default.rb
+++ Shockwave/recipes/default.rb
@@ -1,10 +1,10 @@
 Chef::Log.info("******downloading Shockwave player ******")
-remote_file 'C:/Windows/Temp/Shockwave.msi' do
-  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Shockwave.msi'
+remote_file 'C:/Windows/Temp/shockwave.msi' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/shockwave.msi'
   action :create
 end
 Chef::Log.info("******executing Shockwave installer ******")
 execute 'Shockwave' do
-  command 'C:/Windows/Temp/Shockwave.msi /q /norestart'
+  command 'C:/Windows/Temp/shockwave.msi /q /norestart'
 end
 Chef::Log.info("****** Shockwave install complete ******")
