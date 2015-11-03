6d65f3c88db2dd522b9d69c4937210b4f4e4d998
landmissle <clendenin1@llnl.gov>
2015-11-03 15:41:27 -0800

    Removed msiexec command from OpenDNS installation string.

--- OpenDNS/recipes/default.rb
+++ OpenDNS/recipes/default.rb
@@ -6,7 +6,7 @@ end
 Chef::Log.info("****** OpenDNS Installer download complete ******")
 Chef::Log.info("******Executing OpenDNS installer ******")
 execute 'opendns' do
-  command 'C:/Windows/System32/msiexec /i C:/Windows/Temp/opendns.msi /qn ORG_ID=22002 ORG_FINGERPRINT=664c2b8eda3c9020882016782c8e8803 USER_ID=6764655'
+  command 'C:/Windows/Temp/opendns.msi /qn ORG_ID=22002 ORG_FINGERPRINT=664c2b8eda3c9020882016782c8e8803 USER_ID=6764655'
 end
 Chef::Log.info("****** OpenDNS installation complete .******")
 
