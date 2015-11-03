77d14ec2f6ad653d7a874f1c5999d3f668676a0c
landmissle <clendenin1@llnl.gov>
2015-11-03 14:08:32 -0800

    Updated SEP package installer and altered installation order so that reg key import occurs last.

--- SEPAWS/recipes/default.rb
+++ SEPAWS/recipes/default.rb
@@ -1,14 +1,5 @@
 
-Chef::Log.info("******Downloading Quiet SEP Reg Key ******")
-remote_file 'C:/Windows/Temp/Quiet_SEP.reg' do
-  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Quiet_SEP.reg'
-  action :create
-end
-Chef::Log.info("******Importing Quiet SEP Reg Key ******")
-execute 'sepconfig' do
-  command 'C:/Windows/system32/reg.exe import C:/Windows/Temp/Quiet_SEP.reg'
-end
-Chef::Log.info("******Configuration of SEPAWS Complete.******")
+
 Chef::Log.info("******Downloading SEPAWS installer.******")
 remote_file 'C:/Windows/Temp/sepaws.exe' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/sepaws.exe'
@@ -19,4 +10,13 @@ execute 'sepaws' do
   command 'C:/Windows/Temp/sepaws.exe'
 end
 Chef::Log.info("****** SEPAWS install complete ******")
-
+Chef::Log.info("******Downloading Quiet SEP Reg Key ******")
+remote_file 'C:/Windows/Temp/Quiet_SEP.reg' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Quiet_SEP.reg'
+  action :create
+end
+Chef::Log.info("******Importing Quiet SEP Reg Key ******")
+execute 'sepconfig' do
+  command 'C:/Windows/system32/reg.exe import C:/Windows/Temp/Quiet_SEP.reg'
+end
+Chef::Log.info("******Configuration of SEPAWS Complete.******")
