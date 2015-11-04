9de91b48d8aac6428b03871e523f0c1761b6b550
landmissle <clendenin1@llnl.gov>
2015-11-04 14:09:23 -0800

    Changed Firefox source to not include CCK check.

--- Firefox/recipes/default.rb
+++ Firefox/recipes/default.rb
@@ -1,13 +1,13 @@
-Chef::Log.info("******downloading firefox installer.******")  
+Chef::Log.info("******Downloading Firefox installer.******")  
 remote_file 'C:/Windows/Temp/FirefoxSetup.exe' do  
- source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/LLNL_FF_38_3_0_ESR_wCCK.EXE' 
+ source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Firefox_Setup_38.4.0esr_ENU.exe' 
   action :create
 end
-Chef::Log.info("******executing firefox installer ******")
+Chef::Log.info("******Executing Firefox installer ******")
 execute 'firefox' do
   command 'C:/Windows/Temp/FirefoxSetup.exe'
 end
-Chef::Log.info("****** firefox install complete ******")
+Chef::Log.info("****** Firefox install complete ******")
 Chef::Log.info("******downloading firefox override.ini .******")
 remote_file 'C:/PROGRA~2/MOZILL~1/override.ini' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/override.ini'
@@ -20,12 +20,12 @@ end
 directory 'C:/PROGRA~2/MOZILL~1/browser/defaults/preferences' do
   action :create
 end
-Chef::Log.info("******downloading firefox preferences .******")
+Chef::Log.info("******Downloading Firefox preferences .******")
 remote_file 'C:/PROGRA~2/MOZILL~1/browser/defaults/preferences/AllUsers.js' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/AllUsers.js'
   action :create
 end
-Chef::Log.info("******downloading firefox autoupdate file .******")
+Chef::Log.info("******Downloading Firefox autoupdate file .******")
 remote_file 'C:/PROGRA~2/MOZILL~1/defaults/pref/autoupdate.js' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/autoupdate.js'
   action :create
