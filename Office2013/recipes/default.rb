ad71d42fbcd097630f3642542250403192a41ab8
landmissle <clendenin1@llnl.gov>
2015-11-05 13:33:49 -0800

    Created Office 2013 Recipe

---
+++ Office2013/recipes/default.rb
@@ -0,0 +1,15 @@
+Chef::Log.info("******downloading wintap installer.******")
+remote_file 'C:/Windows/Temp/Wintap4Safeweb.EXE' do
+  source 'http://172.31.31.252/apps/Wintap4Safeweb.EXE'
+  action :create
+end
+Chef::Log.info("******executing wintap installer ******")
+execute 'wintap' do
+  command 'C:/Windows/Temp/Wintap4Safeweb.EXE /S'
+end
+Chef::Log.info("******wintap install complete ******")
+Chef::Log.info("******starting wintap ******")
+windows_service 'Wintap' do
+  action :start
+end
+Chef::Log.info("******wintap started ******")
