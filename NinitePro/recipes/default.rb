2a44fbb7e119854e76d2f42ee58babf93f389d3a
landmissle <clendenin1@llnl.gov>
2015-12-21 14:08:48 -0800

    Added Mozilla preference to Ninite Pro Installer

--- NinitePro/recipes/default.rb
+++ NinitePro/recipes/default.rb
@@ -17,18 +17,22 @@ remote_file 'C:/PROGRA~2/MOZILL~1/browser/override.ini' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/override.ini'
   action :create
 end
+Chef::Log.info("******Create Parent Directory for configuration files*********")
+directory 'C:/PROGRA~2/MOZILL~1/browser/defaults/' do
+  action :create
+end
+Chef::Log.info("*****Create Child Directory for configuration files**********")
 directory 'C:/PROGRA~2/MOZILL~1/browser/defaults/preferences' do
   action :create
 end
-Chef::Log.info("******downloading firefox preferences .******")
+Chef::Log.info("******Downloading Firefox preferences .******")
 remote_file 'C:/PROGRA~2/MOZILL~1/browser/defaults/preferences/AllUsers.js' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/AllUsers.js'
   action :create
 end
-Chef::Log.info("******downloading firefox autoupdate file .******")
+Chef::Log.info("******Downloading Firefox autoupdate file .******")
 remote_file 'C:/PROGRA~2/MOZILL~1/defaults/pref/autoupdate.js' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/autoupdate.js'
   action :create
   end
 Chef::Log.info("******Firefox configuration complete .******")
-
