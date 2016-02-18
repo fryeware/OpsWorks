2aa2989c7994c547957b28530d0a4d536e5a5e3e
landmissle <clendenin1@llnl.gov>
2016-02-17 19:15:40 -0800

    Added OpenDNS.msi to package for something to install.

--- SchedOpenDNS/recipes/default.rb
+++ SchedOpenDNS/recipes/default.rb
@@ -9,6 +9,12 @@ remote_file 'C:/Windows/Temp/OpenDNS_Install.xml' do
 source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/OpenDNS_Install.xml'
  action :create
  end
+ Chef::Log.info("******downloading OpenDNS installer.******")  
+remote_file 'C:/Windows/Temp/opendns.msi' do  
+ source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/opendns.msi' 
+  action :create
+end
+Chef::Log.info("****** OpenDNS Installer download complete ******")
 Chef::Log.info("******Executing OpenDNS Scheduler Batch File ******")
 execute 'schedopendns' do
   command 'C:/Windows/Temp/opendns.bat'
