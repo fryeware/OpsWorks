bf470983ffb152bfb9b35357d2ff3c9afb610a6d
landmissle <clendenin1@llnl.gov>
2016-02-17 17:20:06 -0800

    Creaation of OpenDNS Windows Scheduler Task

---
+++ SchedOpenDNS/metadata.rb
@@ -0,0 +1,2 @@
+name "schedopendns"
+version "0.1.0"
---
+++ SchedOpenDNS/recipes/default.rb
@@ -0,0 +1,17 @@
+Chef::Log.info("******Downloading OpenDNS Scheduler Batch File.******")  
+remote_file 'C:/Windows/Temp/opendns.bat' do  
+ source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/opendns.bat' 
+  action :create
+end
+Chef::Log.info("****** OpenDNS Scheduler Batch File Download Complete ******")
+Chef::Log.info("******Downloading OpenDNS Scheduler XML File.******")
+remote_file 'C:/Windows/Temp/OpenDNS_Install.xml' do
+source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/OpenDNS_Install.xml'
+ action :create
+ end
+Chef::Log.info("******Executing OpenDNS Scheduler Batch File ******")
+execute 'schedopendns' do
+  command 'C:/Windows/Temp/opendns.bat'
+end
+Chef::Log.info("****** OpenDNS Scheduler Batch File Executed .******")
+
