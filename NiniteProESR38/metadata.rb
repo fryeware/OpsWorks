37dab185fd3502b529ceaf8b87fdb6dad04d8d8c
landmissle <clendenin1@llnl.gov>
2016-04-19 21:01:55 -0700

    New Recipe for NinitePro for Firefox issue. Need to specficy ESR 38 for now.

---
+++ NiniteProESR38/metadata.rb
@@ -0,0 +1,2 @@
+name "niniteproesr38"
+version "0.1.0"
---
+++ NiniteProESR38/recipes/default.rb
@@ -0,0 +1,38 @@
+Chef::Log.info("******downloading NinitePro installer.******")
+remote_file 'C:/Windows/Temp/NinitePro.exe' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/NinitePro.exe'
+  action :create
+end
+Chef::Log.info("******executing NiniePro installer ******")
+execute 'niniteprotrial' do
+  command 'C:/Windows/Temp/NinitePro.exe /select Chrome "Firefox ESR 38" Flash QuickTime Reader Shockwave Silverlight /silent c:/Windows/Temp/report.txt'
+end
+Chef::Log.info("****** NinitePro install complete ******")
+Chef::Log.info("******downloading firefox override.ini .******")
+remote_file 'C:/PROGRA~2/MOZILL~1/override.ini' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/override.ini'
+  action :create
+end
+remote_file 'C:/PROGRA~2/MOZILL~1/browser/override.ini' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/override.ini'
+  action :create
+end
+Chef::Log.info("******Create Parent Directory for configuration files*********")
+directory 'C:/PROGRA~2/MOZILL~1/browser/defaults/' do
+  action :create
+end
+Chef::Log.info("*****Create Child Directory for configuration files**********")
+directory 'C:/PROGRA~2/MOZILL~1/browser/defaults/preferences' do
+  action :create
+end
+Chef::Log.info("******Downloading Firefox preferences .******")
+remote_file 'C:/PROGRA~2/MOZILL~1/browser/defaults/preferences/AllUsers.js' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/AllUsers.js'
+  action :create
+end
+Chef::Log.info("******Downloading Firefox autoupdate file .******")
+remote_file 'C:/PROGRA~2/MOZILL~1/defaults/pref/autoupdate.js' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/autoupdate.js'
+  action :create
+  end
+Chef::Log.info("******Firefox configuration complete .******")
