8c179c5fe97417eb4a64381f089bd2a5dfecad12
landmissle <clendenin1@llnl.gov>
2016-07-27 15:34:41 -0700

    Reverted installion package and registry (in that order). Updated SEP to not haver tamper protection.

--- SEPAWSTEST/recipes/default.rb
+++ SEPAWSTEST/recipes/default.rb
@@ -5,6 +5,11 @@ remote_file 'C:/Windows/Temp/sepclienttest.exe' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SepClientTest/sepclienttest.exe'
   action :create
 end
+Chef::Log.info("******Executing SEPClientTest installer ******")
+execute 'sepclienttest' do
+  command 'C:/Windows/Temp/sepclienttest.exe'
+end
+Chef::Log.info("****** SEPClientTest install complete ******")
 Chef::Log.info("******Downloading Quiet SEP Reg Key ******")
 remote_file 'C:/Windows/Temp/Quiet_SEP.reg' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Quiet_SEP.reg'
@@ -14,9 +19,4 @@ Chef::Log.info("******Importing Quiet SEP Reg Key ******")
 execute 'sepconfig' do
   command 'C:/Windows/system32/reg.exe import C:/Windows/Temp/Quiet_SEP.reg'
 end
-Chef::Log.info("******Executing SEPClientTest installer ******")
-execute 'sepclienttest' do
-  command 'C:/Windows/Temp/sepclienttest.exe'
-end
-Chef::Log.info("****** SEPClientTest install complete ******")
 Chef::Log.info("******Configuration of SEPClientTest Complete.******")
