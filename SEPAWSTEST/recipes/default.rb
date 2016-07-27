151c7d7a9c4da38aaff15e89b466f5afc9456a62
landmissle <clendenin1@llnl.gov>
2016-07-27 14:44:25 -0700

    Run reg import first. Updated SEP client to include tamper protection

--- SEPAWSTEST/recipes/default.rb
+++ SEPAWSTEST/recipes/default.rb
@@ -5,11 +5,6 @@ remote_file 'C:/Windows/Temp/sepclienttest.exe' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SepClientTest/sepclienttest.exe'
   action :create
 end
-Chef::Log.info("******Executing SEPClientTest installer ******")
-execute 'sepclienttest' do
-  command 'C:/Windows/Temp/sepclienttest.exe /ni'
-end
-Chef::Log.info("****** SEPClientTest install complete ******")
 Chef::Log.info("******Downloading Quiet SEP Reg Key ******")
 remote_file 'C:/Windows/Temp/Quiet_SEP.reg' do
   source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Quiet_SEP.reg'
@@ -19,4 +14,9 @@ Chef::Log.info("******Importing Quiet SEP Reg Key ******")
 execute 'sepconfig' do
   command 'C:/Windows/system32/reg.exe import C:/Windows/Temp/Quiet_SEP.reg'
 end
+Chef::Log.info("******Executing SEPClientTest installer ******")
+execute 'sepclienttest' do
+  command 'C:/Windows/Temp/sepclienttest.exe'
+end
+Chef::Log.info("****** SEPClientTest install complete ******")
 Chef::Log.info("******Configuration of SEPClientTest Complete.******")
