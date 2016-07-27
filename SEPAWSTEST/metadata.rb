37950211d023aac2ad88062e3c95e407d40d5cc5
landmissle <clendenin1@llnl.gov>
2016-07-27 11:03:43 -0700

    Uploading SEP 12.1.6 MP5

---
+++ SEPAWSTEST/metadata.rb
@@ -0,0 +1,2 @@
+name "sepawstest"
+version "0.1.0"
---
+++ SEPAWSTEST/recipes/default.rb
@@ -0,0 +1,22 @@
+
+
+Chef::Log.info("******Downloading SEPClientTest installer.******")
+remote_file 'C:/Windows/Temp/sepclienttest.exe' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SepClientTest/sepclienttest.exe'
+  action :create
+end
+Chef::Log.info("******Executing SEPClientTest installer ******")
+execute 'sepclienttest' do
+  command 'C:/Windows/Temp/sepclienttest.exe /ni'
+end
+Chef::Log.info("****** SEPClientTest install complete ******")
+Chef::Log.info("******Downloading Quiet SEP Reg Key ******")
+remote_file 'C:/Windows/Temp/Quiet_SEP.reg' do
+  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Quiet_SEP.reg'
+  action :create
+end
+Chef::Log.info("******Importing Quiet SEP Reg Key ******")
+execute 'sepconfig' do
+  command 'C:/Windows/system32/reg.exe import C:/Windows/Temp/Quiet_SEP.reg'
+end
+Chef::Log.info("******Configuration of SEPClientTest Complete.******")
