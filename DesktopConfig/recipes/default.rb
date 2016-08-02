4b713ffc2cfa705e3bba8dab0e5a890797bac273
landmissle <clendenin1@llnl.gov>
2016-08-02 11:01:41 -0700

    Changed format of PowerShell command to conform to Chef Recipe

--- DesktopConfig/recipes/default.rb
+++ DesktopConfig/recipes/default.rb
@@ -1,10 +1,28 @@
-Chef::Log.info("******Downloading Desktop Configuration Powershell Script******")
-remote_file 'C:/Windows/Temp/desktopconfig.ps1' do
-  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/Desktops/desktopconfig.ps1'
-  action :create
+Chef::Log.info("******Running Powershell commands to configure network and join AD domain ******")
+
+powershell_script "Run Desktopconfig" do
+    code <<-EOH
+    
+# Configure PowerShell to accept remote commands.
+set-executionpolicy remotesigned -Force;
+
+# Configure DNS network settings
+Set-DNSClientServerAddress -interfaceIndex 12 -ServerAddresses ("172.31.43.98","172.31.37.33");
+
+# Set Timezone
+tzutil /s "Pacific Standard Time";
+
+# Install the Desktop Experience feature
+Install-WindowsFeature  Desktop-Experience;
+
+# Credentials for joining system to AD domain
+$cred = New-Object System.Management.Automation.PsCredential("Safeweb\safetydance", (ConvertTo-SecureString "Pass4saf" -AsPlainText -Force));
+$cred.GetNetworkCredential().Password;
+
+# Add system to AD domain and reboot
+Add-Computer -DomainName "Safeweb.AWS.Net" -OUPath "OU=SessionHosts,DC=safeweb,DC=aws,DC=net" -Credential $cred;
+Restart-Computer;
+
+EOH
+guard_interpreter :desktopconfig
 end
-Chef::Log.info("******Executing DesktopConfig PowerShell Script ******")
-execute 'desktopconfig' do
-  command 'C:/Windows/Temp/desktopconfig.ps1'
-end
-Chef::Log.info("******Desktop Configuration PS Script complete ******")
