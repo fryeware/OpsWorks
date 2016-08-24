94b737b9041e0e3efdd2b95fa28bf08fcb621088
landmissle <clendenin1@llnl.gov>
2016-08-24 12:19:01 -0700

    Added " " around path location

--- PSOffice2013/recipes/default.rb
+++ PSOffice2013/recipes/default.rb
@@ -4,7 +4,7 @@ powershell_script "Install PSOffice2013" do
   code <<-EOH
     $cred = New-Object System.Management.Automation.PsCredential("Safeweb-upd1\scribe", (ConvertTo-SecureString "Pass4scr" -AsPlainText -Force));
 
-New-PSDrive -PSProvider Filesystem -Name office2013 -Root \\172.31.43.194\office -Credential $cred;
+New-PSDrive -PSProvider Filesystem -Name office2013 -Root "\\172.31.43.194\office" -Credential $cred;
 
 office2013:\x64\setup.exe;
   EOH
