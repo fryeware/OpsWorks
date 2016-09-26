f569cda4adef12d5cd18264c5ce984a0f7200d60
landmissle <clendenin1@llnl.gov>
2016-09-26 11:15:42 -0700

    Removed # comments and added "guard interpreter" line.

--- PSADJoin/recipes/default.rb
+++ PSADJoin/recipes/default.rb
@@ -2,10 +2,9 @@ Chef::Log.info("******PS AD enrollement into Safeweb Domain******")
 
 powershell_script "Run AD enrollment PS script" do
   code <<-EOH
-#Provde Credentials for PS session
 $cred = New-Object System.Management.Automation.PsCredential("safeweb\safetydance", (ConvertTo-SecureString "Pass4saf" -AsPlainText -Force));
-# Enroll system into Safeweb Domain and reboot system
 Add-Computer -DomainName "Safeweb.AWS.Net" -OUPath "OU=SessionHosts,DC=safeweb,DC=aws,DC=net" -Credential $cred;
 Restart-Computer;
   EOH
+  guard_interpreter :powershell_script
   end
