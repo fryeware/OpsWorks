a1ddbba811898cb6451892a2f3e6cb70ffd58ff3
landmissle <clendenin1@llnl.gov>
2016-09-26 14:52:49 -0700

    Added DC DNS entry for NIC

--- PSADJoin/recipes/default.rb
+++ PSADJoin/recipes/default.rb
@@ -2,6 +2,7 @@ Chef::Log.info("******PS AD enrollement into Safeweb Domain******")
 
 powershell_script "Run AD enrollment PS script" do
   code <<-EOH
+Set-DNSClientServerAddress -interfaceIndex 12 -ServerAddresses ("172.31.43.98","172.31.37.33");
 $cred = New-Object System.Management.Automation.PsCredential("safeweb\safetydance", (ConvertTo-SecureString "Pass4saf" -AsPlainText -Force));
 Add-Computer -DomainName "Safeweb.AWS.Net" -OUPath "OU=SessionHosts,DC=safeweb,DC=aws,DC=net" -Credential $cred;
 Restart-Computer;
