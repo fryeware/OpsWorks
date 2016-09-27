62851ce41d91d66d0fa39ad8ad0f15cdab1299c6
landmissle <clendenin1@llnl.gov>
2016-09-27 11:13:43 -0700

    Added native Ruby code for AD enrollement.

--- PSADJoin/recipes/default.rb
+++ PSADJoin/recipes/default.rb
@@ -1,11 +1,17 @@
 Chef::Log.info("******PS AD enrollement into Safeweb Domain******")
 
-powershell_script "Run AD enrollment PS script" do
+powershell_script "Set NIC to Safeweb DC for DNS Resolution" do
   code <<-EOH
 Set-DNSClientServerAddress -interfaceIndex 12 -ServerAddresses ("172.31.43.98","172.31.37.33");
-$cred = New-Object System.Management.Automation.PsCredential("safeweb\safetydance", (ConvertTo-SecureString "Pass4saf" -AsPlainText -Force));
-Add-Computer -DomainName "Safeweb.AWS.Net" -OUPath "OU=SessionHosts,DC=safeweb,DC=aws,DC=net" -Credential $cred;
-Restart-Computer;
   EOH
   guard_interpreter :powershell_script
   end
+  Chef::Log.info(****Join Computer to Safeweb Domain)
+  # Join Safeweb domain with OU
+windows_ad_computer "safeweb.aws.net" do
+  action :join
+  domain_pass 'Pass4saf'
+  domain_user 'safetydance'
+  domain_name 'safeweb.aws.net'
+  ou "SessionHosts"
+end
