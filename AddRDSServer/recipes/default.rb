c9290a7cb11bf7a30718dd6b125260f119590e91
landmissle <clendenin1@llnl.gov>
2016-10-28 13:38:22 -0700

    Fixed various syntax errors

--- AddRDSServer/recipes/default.rb
+++ AddRDSServer/recipes/default.rb
@@ -1,11 +1,11 @@
 Chef::Log.info("******Downloading Powershell RDSAddServer script to join AD and prep for RDS ******")
 
-remote_file 'C:/Windows/Temp/RDSInstallRecipe.ps1' do
+remote_file 'C:/Windows/Temp/RDSAddServer.ps1' do
 source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/LinuxDev/RDSAddServer.ps1'
 action :create
 end
 Chef::Log.info("******Executing PowerShell Script ******")
-execute 'desktopconfig' do
+execute 'RDSAddServer' do
 command 'powershell -f C:/Windows/Temp/RDSAddServer.ps1'
 end
 Chef::Log.info("******RDSAddServer PS Script complete ******")
