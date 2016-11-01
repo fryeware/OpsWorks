72d95f3185a2e74e1444f1c03892bf00e014adb0
landmissle <clendenin1@llnl.gov>
2016-11-01 14:53:36 -0700

    Moved download and source directory to Windows/temp on local system.

--- FlashPAPPI/recipes/default.rb
+++ FlashPAPPI/recipes/default.rb
@@ -1,10 +1,10 @@
 Chef::Log.info("******Downloading FlashPAPI installer.******")  
-remote_file 'C:/cert/FlashPAPPI.ps1' do  
+remote_file 'C:/Windows/temp/FlashPAPPI.ps1' do  
  source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/FlashPAPPI.ps1' 
   action :create
 end
 Chef::Log.info("******Executing FlashPAPPI installer ******")
 execute 'FlashPAPPI' do
-command 'powershell -f C:/cert/FlashPAPPI.ps1'
+command 'powershell -f C:/Windows/temp/FlashPAPPI.ps1'
 end
 Chef::Log.info("****** FlashPAPPI install complete ******")
