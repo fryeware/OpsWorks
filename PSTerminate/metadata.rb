a5c8164ea391bccebfa33a2f7ad0cd60dafa4b47
landmissle <clendenin1@llnl.gov>
2016-11-10 15:40:12 -0800

    Terminate Instance upon shutdown PS script created.

---
+++ PSTerminate/metadata.rb
@@ -0,0 +1,2 @@
+name "PSTerminate"
+version "0.1.0"
---
+++ PSTerminate/recipes/default.rb
@@ -0,0 +1,8 @@
+Chef::Log.info("******Send Eventid to trigger terminate scheduled task******")
+
+powershell_script "PSTerminate" do
+  code <<-EOH
+    Write-EventLog -LogName Application -Source "AWS Shutdown and Destroy Instance" -EntryType Information -EventId 5150 -Message "Shutting down instance and teminating it."
+  EOH
+  guard_interpreter :powershell_script
+  end
