27531176e5b331adbdafad86a0244fcf971d277c
landmissle <clendenin1@llnl.gov>
2016-11-10 16:51:01 -0800

    This recipe will now create scheduled task that terminates instance

--- RDSRemoveDestroy/recipes/default.rb
+++ RDSRemoveDestroy/recipes/default.rb
@@ -1,10 +1,10 @@
 Chef::Log.info("******Downloading CollectionRemovalDestroyInstance Powershell script.******")  
-remote_file 'C:/Windows/temp/CollectionRemovalDestroyInstance.ps1' do  
- source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/LinuxDev/CollectionRemovalDestroyInstance.ps1' 
+remote_file 'C:/Windows/temp/ScedTerminate.ps1' do  
+ source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/LinuxDev/SchedTerminate.ps1' 
   action :create
 end
-Chef::Log.info("******Executing CollectionRemovalDestroyInstance Powershell script. ******")
+Chef::Log.info("******Executing SchedTerminate Powershell script. ******")
 execute 'RDSRemoveDestroy' do
-command 'powershell -f C:/Windows/temp/CollectionRemovalDestroyInstance.ps1'
+command 'powershell -f C:/Windows/temp/ScedTerminate.ps1'
 end
-Chef::Log.info("****** CollectionRemovalDestroyInstance Powershell script complete ******")
+Chef::Log.info("****** Scheduled Task  Created to Remove and Terminate******")
