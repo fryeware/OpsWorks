941d8a44c038f564693c6996485dbfb93f8d89c6
landmissle <clendenin1@llnl.gov>
2016-11-14 09:30:11 -0800

    Added shutdown command with 180 second delay

--- PSTerminate/recipes/default.rb
+++ PSTerminate/recipes/default.rb
@@ -2,7 +2,8 @@ Chef::Log.info("******Send Eventid to trigger terminate scheduled task******")
 
 powershell_script "PSTerminate" do
   code <<-EOH
-    Write-EventLog -LogName Application -Source "AWS Shutdown and Destroy Instance" -EntryType Information -EventId 5150 -Message "Shutting down instance and teminating it."
+    Write-EventLog -LogName Application -Source "AWS Shutdown and Destroy Instance" -EntryType Information -EventId 5150 -Message "Shutting down instance and teminating it.";
+	'c:/windows/system32/shutdown -t 180';
   EOH
   guard_interpreter :powershell_script
   end
