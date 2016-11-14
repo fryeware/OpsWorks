c09a7f7e5d78bbfffeacaf98227cd836299c95f9
landmissle <clendenin1@llnl.gov>
2016-11-14 11:00:38 -0800

    Removed Shutdown.exe command.

--- PSTerminate/recipes/default.rb
+++ PSTerminate/recipes/default.rb
@@ -3,7 +3,6 @@ Chef::Log.info("******Send Eventid to trigger terminate scheduled task******")
 powershell_script "PSTerminate" do
   code <<-EOH
     Write-EventLog -LogName Application -Source "AWS Shutdown and Destroy Instance" -EntryType Information -EventId 5150 -Message "Shutting down instance and teminating it.";
-	'c:/windows/system32/shutdown.exe -t 180';
   EOH
   guard_interpreter :powershell_script
   end
