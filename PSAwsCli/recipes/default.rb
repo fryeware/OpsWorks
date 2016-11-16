df742b4ebea3a20bce7497feb483c5c3cd584c04
landmissle <clendenin1@llnl.gov>
2016-11-16 14:37:16 -0800

    Replaced \ backslash with / forward slash

--- PSAwsCli/recipes/default.rb
+++ PSAwsCli/recipes/default.rb
@@ -2,8 +2,8 @@ Chef::Log.info("******AWS CLI******")
 
 powershell_script "PSAwsCli" do
   code <<-EOH
-  Invoke-WebRequest https://s3.amazonaws.com/aws-cli/AWSCLI64.msi -OutFile C:\Windows\Temp\AWSCLI64.msi;
-  Start-Process "c:\Windows\temp\AWSCLI64.msi" /qn -Wait;
+  Invoke-WebRequest https://s3.amazonaws.com/aws-cli/AWSCLI64.msi -OutFile C:/Windows/Temp/AWSCLI64.msi;
+  Start-Process "c:/Windows/temp/AWSCLI64.msi" /qn -Wait;
    EOH
   guard_interpreter :powershell_script
 end
