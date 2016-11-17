f03ffc8f13ccc41dd2af5936e81129008db72cf9
landmissle <clendenin1@llnl.gov>
2016-11-17 15:46:53 -0800

    Inserted register-sched command and altered embedded slashes to \ backslashes.

--- PSNoConnectsEveSW/recipes/default.rb
+++ PSNoConnectsEveSW/recipes/default.rb
@@ -4,6 +4,7 @@ powershell_script "PSNoConnectsEveSW" do
   code <<-EOH
 Invoke-WebRequest https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/NoConnectsSW.ps1 -OutFile c:/Windows/Temp/NoConnectsSW.ps1;
 Invoke-WebRequest https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/NoConnectsSW.xml -OutFile c:/Windows/Temp/NoConnectsSW.xml;
+Register-ScheduledTask -Xml (get-content 'c:\Windows\Temp\NoConnectsSW.xml' | out-string) -TaskName "No Evening Connections for Safeweb" -User safeweb\safetydance -Password Pass4saf –Force;
     EOH
   guard_interpreter :powershell_script
   end
