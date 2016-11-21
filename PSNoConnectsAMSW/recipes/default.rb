75a93c80d187063b3d82c86989ce74bbbe49e570
landmissle <clendenin1@llnl.gov>
2016-11-21 15:08:15 -0800

    Correct AM file names in PS and XML files

--- PSNoConnectsAMSW/recipes/default.rb
+++ PSNoConnectsAMSW/recipes/default.rb
@@ -5,8 +5,8 @@ Chef::Log.info("******Creating No Connect AM Scheduled Task******")
 powershell_script "PSNoConnectsAMSW" do
   code <<-EOH
 Invoke-WebRequest https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/NoConnectsSW.ps1 -OutFile c:/Windows/Temp/NoConnectsSW.ps1;
-Invoke-WebRequest https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/NoConnectsSW.xml -OutFile c:/Windows/Temp/NoConnectsAMSW.xml;
-Register-ScheduledTask -Xml (get-content c:/Windows/Temp/NoConnectsSW.xml | out-string) -TaskName \"No Evening Connections for Safeweb\" -User safeweb\\safetydance -Password Pass4saf –Force;
+Invoke-WebRequest https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/NoConnectsAMSW.xml -OutFile c:/Windows/Temp/NoConnectsAMSW.xml;
+Register-ScheduledTask -Xml (get-content c:/Windows/Temp/NoConnectsAMSW.xml | out-string) -TaskName \"No Morning Connections for Safeweb\" -User safeweb\\safetydance -Password Pass4saf –Force;
     EOH
   guard_interpreter :powershell_script
   end
