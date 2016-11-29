cc5d2e0dac4aef67e7137d6831d5e7ead37a2ba3
landmissle <clendenin1@llnl.gov>
2016-11-29 09:59:35 -0800

    Hand typed "register-scheduledtask" line. No cut n' paste

--- PSLogOutRunt/recipes/default.rb
+++ PSLogOutRunt/recipes/default.rb
@@ -6,8 +6,8 @@ powershell_script "PSLogOutRunt" do
   code <<-EOH
 Invoke-WebRequest https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/PSLogOutSW.ps1 -OutFile c:/Windows/Temp/PSLogOutSW.ps1;
 Invoke-WebRequest https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/PSLogOutRunt.xml -OutFile c:/Windows/Temp/PSLogOutRunt.xml;
-Register-ScheduledTask -Xml (get-content c:/Windows/Temp/PSLogOutRunt.xml | out-string) -TaskName \"Log Out RDS User Connections for Safeweb\" -User safeweb\\safetydance -Password Pass4saf –Force;
-    EOH
+Register-ScheduledTask -Xml (get-content c:/Windows/Temp/PSLogOutRunt.xml | out-string) -TaskName \"Log Out RDS User Connections for Safeweb\" -User safeweb\\safetydance -Password Pass4saf -Force;
+EOH
   guard_interpreter :powershell_script
   end
   Chef::Log.info("******Evening Logout Scheduled Task Created******")
