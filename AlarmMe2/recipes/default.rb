b02c8259dbbaef078ecd60909734b515b3b2d258
landmissle <clendenin1@llnl.gov>
2016-12-02 11:08:02 -0800

    Changed AlarmMe.ps1 to AlarmMe2.ps1

--- AlarmMe2/recipes/default.rb
+++ AlarmMe2/recipes/default.rb
@@ -1,5 +1,5 @@
 Chef::Log.info("******Downloading Powershell AlarmMe script to creat AWS CloudWatch Alarm ******")
-remote_file 'C:/Windows/Temp/AlarmMe.ps1' do
+remote_file 'C:/Windows/Temp/AlarmMe2.ps1' do
 source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/AlarmMe.ps1'
 action :create
 end
@@ -7,6 +7,6 @@ Chef::Log.info("******Download Complete for Powershell script to create AWS Clou
 
 Chef::Log.info("******Executing PowerShell Script to create AWS CloudWatch Alarm******")
 execute 'AlarmMe2' do
-command 'powershell -f C:/Windows/Temp/AlarmMe.ps1'
+command 'powershell -f C:/Windows/Temp/AlarmMe2.ps1'
 end
 Chef::Log.info("******Powershell script for AWS CloudWatch Alarm completed******")
