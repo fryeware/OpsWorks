a4d30cabad7169690f4da7b1b85aadf52ca598b7
landmissle <clendenin1@llnl.gov>
2016-12-02 12:00:58 -0800

    powershell -file \"c:.....\"

--- AlarmMe2/recipes/default.rb
+++ AlarmMe2/recipes/default.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 # encoding: Windows-1252
-Chef::Log.info("******Creating Evening Logout Scheduled Task ******")
+Chef::Log.info("******Download AWS CloudWatch Alarm Script ******")
 
-powershell_script "PSLogOutRunt" do
+powershell_script "AlarmMe2" do
   code <<-EOH
 Invoke-WebRequest https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/AlarmMe.ps1 -OutFile c:/Windows/Temp/AlarmMe.ps1;
-c:/Windows/Temp/AlarmMe.ps1
+powershell.exe -file \"c:/Windows/Temp/AlarmMe.ps1\"
 EOH
   guard_interpreter :powershell_script
   end
