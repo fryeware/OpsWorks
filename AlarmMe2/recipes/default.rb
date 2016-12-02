f3f96861de0bd1d3db3ce7a276624db195781c88
landmissle <clendenin1@llnl.gov>
2016-12-02 10:27:56 -0800

    Change for scheduled task to direct PS script launch.

--- AlarmMe2/recipes/default.rb
+++ AlarmMe2/recipes/default.rb
@@ -1,16 +1,12 @@
 Chef::Log.info("******Downloading Powershell AlarmMe script to creat AWS CloudWatch Alarm ******")
 remote_file 'C:/Windows/Temp/AlarmMe.ps1' do
 source 'https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/AlarmMe.ps1'
-#!/usr/bin/env ruby
-# encoding: Windows-1252
-Chef::Log.info("******Creating AlarmMe2 Scheduled Task ******")
+action :create
+end
+Chef::Log.info("******Download Complete for Powershell script to create AWS CloudWatch Alarm******")
 
-powershell_script "AlarmMe2" do
-  code <<-EOH
-Invoke-WebRequest https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/AlarmMe.ps1 -OutFile c:/Windows/Temp/AlarmMe.ps1;
-Invoke-WebRequest https://s3-us-west-2.amazonaws.com/safeweb-application-repo-test/SafewebPS/AlarmMe.xml -OutFile c:/Windows/Temp/AlarmMe.xml;
-Register-ScheduledTask -Xml (get-content c:/Windows/Temp/AlarmMe.xml | out-string) -TaskName \"Create AWS Alarm in CloudWatch\"
-    EOH
-  guard_interpreter :powershell_script
-  end
-  Chef::Log.info("******Completed AlarmMe2 Scheduled Task Created******")
+Chef::Log.info("******Executing PowerShell Script to create AWS CloudWatch Alarm******")
+execute 'AlarmMe2' do
+command 'powershell -f C:/Windows/Temp/AlarmMe.ps1'
+end
+Chef::Log.info("******Powershell script for AWS CloudWatch Alarm completed******")
