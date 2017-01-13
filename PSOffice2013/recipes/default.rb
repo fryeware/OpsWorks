88f87d25303e90759680ce4c7deb4a4dbe0fa5f9
landmissle <clendenin1@llnl.gov>
2017-01-13 15:05:29 -0800

    Changed network drive to P: and used //

--- PSOffice2013/recipes/default.rb
+++ PSOffice2013/recipes/default.rb
@@ -1,12 +1,14 @@
+#!/usr/bin/env ruby
+# encoding: Windows-1252
 Chef::Log.info("******Installing PSOffice2013******")
 
 powershell_script "Install PSOffice2013" do
   code <<-EOH
-    $cred = New-Object System.Management.Automation.PsCredential("Safeweb-upd1\scribe", (ConvertTo-SecureString "Pass4scr" -AsPlainText -Force));
+    $cred = New-Object System.Management.Automation.PsCredential("Safeweb-upd1\\scribe", (ConvertTo-SecureString "Pass4scr" -AsPlainText -Force));
 
-New-PSDrive -PSProvider Filesystem -Name office2013 -Root "\\172.31.43.194\office" -Credential $cred;
+New-PSDrive -PSProvider Filesystem -Name P -Root "//172.31.43.194/office" -Credential $cred;
 
-office2013:\x64\setup.exe;
+P:/x64/setup.exe;
   EOH
   guard_interpreter :powershell_script
   end
