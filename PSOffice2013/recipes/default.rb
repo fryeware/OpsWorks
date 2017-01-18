aee8ded335bdcd9bf8c71ddb8c23529a54fcdd60
landmissle <clendenin1@llnl.gov>
2017-01-17 16:06:36 -0800

    Added Set-Location

--- PSOffice2013/recipes/default.rb
+++ PSOffice2013/recipes/default.rb
@@ -7,7 +7,7 @@ powershell_script "Install PSOffice2013" do
     $cred = New-Object System.Management.Automation.PsCredential("Safeweb-upd1\\scribe", (ConvertTo-SecureString "Pass4scr" -AsPlainText -Force));
 
 New-PSDrive -PSProvider Filesystem -Name P -Root "//172.31.43.194/office" -Credential $cred;
-
+Set-Location -Path 'P:/x64/'
 &'P:/x64/setup.exe';
   EOH
   guard_interpreter :powershell_script
